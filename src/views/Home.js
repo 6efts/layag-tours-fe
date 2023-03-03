1a3427c2c13a00d62645f0e431780b6210359a56
Vince <vince.dehonor1@gmail.com>
2023-03-03 08:27:58 +0800

    Initial data

---
+++ src/views/Home.js
@@ -0,0 +1,15 @@
+import Banner from "../components/Home Component/Banner";
+import TopTours from "../components/Home Component/TopTours";
+import TourPackages from "../components/Home Component/TourPackages";
+
+function Home() {
+  return (
+    <main>
+      <Banner />
+      <TopTours />
+      <TourPackages />
+    </main>
+  );
+}
+
+export default Home;
